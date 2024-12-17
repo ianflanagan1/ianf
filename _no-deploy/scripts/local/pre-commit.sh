830f1e6e3d2b5847825fb1518fc7436c0f93502a
Ian Flanagan <ianflanagan1@gmail.com>
2024-12-17 08:36:48 +0000

    Fix tests

--- _no-deploy/scripts/local/pre-commit.sh
+++ _no-deploy/scripts/local/pre-commit.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+source "$(dirname "${BASH_SOURCE[0]}")/../../../_gitignore/project-config"
+
 ./vendor/bin/pint --dirty -v
-php artisan test --coverage --min=70
+php artisan test --coverage --min="$TEST_COVERAGE_LIMIT"
 php artisan dusk
