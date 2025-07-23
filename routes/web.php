7e1938a202346d3edcd4f7a70d392ef35e6a1c07
Ian Flanagan <ianflanagan1@gmail.com>
2025-07-23 12:41:33 +0100

    Added route for api-video (redirection to YouTube video)

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -9,17 +8,6 @@ Route::get('/', function () {
 Route::get('/nica', function () {
     return view('nica');
 });
-
-/*
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+Route::get('/api-video', function () {
+    return redirect()->away('https://youtu.be/G7Nug1Mr9VE', 301);
 });
-
-require __DIR__.'/auth.php';
-*/
